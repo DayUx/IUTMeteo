f7e21a69a4ab6403cd04295fb526cfcc56a6658b
Obsyky <quentinkalmar@hotmail.fr>
2022-01-12 12:45:55 +0100

    Update js

--- js/global.js
+++ js/global.js
@@ -1,3 +1,9 @@
 function search(){
-
+    var searchId = document.getElementById('search').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchId},fr&appid=ee07e2bf337034f905cde0bdedae3db8`).then(
+            response => response.json()).then(
+                data => {
+        console.log(data);
+    })
 }
+
