8871996680601bdeef2747dcdbb9cbf28049cb19
Obsyky <quentinkalmar@hotmail.fr>
2022-01-13 16:49:16 +0100

    Update js

--- js/global.js
+++ js/global.js
@@ -1,11 +1,11 @@
 function search(){
     console.log("test")
     var searchId = document.getElementById('search').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchId},fr&appid=ee07e2bf337034f905cde0bdedae3db8`).then(
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${searchId},fr&units=metric&cnt=7&appid=ee07e2bf337034f905cde0bdedae3db8`).then(
             response => response.json()).then(
                 data => {
-                    document.getElementById("ville").innerHTML=data.name;
-                    document.getElementById("pays").innerHTML=data.sys.country;
+                    document.getElementById("ville").innerHTML=data.city.name;
+                    document.getElementById("pays").innerHTML=data.city.country;
 
                     console.log(data);
     })
