e3714602e783f138594022fa81ea7e284643240d
Obsyky <quentinkalmar@hotmail.fr>
2022-01-13 17:01:00 +0100

    Update js

--- js/global.js
+++ js/global.js
@@ -11,3 +11,17 @@ function search(){
     })
 }
 
+function position(position) {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&cnt=7&appid=ee07e2bf337034f905cde0bdedae3db8`).then(
+        response => response.json()).then(
+        data => {
+            document.getElementById("ville").innerHTML=data.city.name;
+            document.getElementById("pays").innerHTML=data.city.country;
+
+            console.log(data);
+        })
+}
+
+if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(position);
+
