55ff2679f28bf75273353d7f3348b62d5d6974aa
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2022-01-13 15:36:14 +0100

    update css

--- js/global.js
+++ js/global.js
@@ -1,9 +1,13 @@
 function search(){
+    console.log("test")
     var searchId = document.getElementById('search').value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchId},fr&appid=ee07e2bf337034f905cde0bdedae3db8`).then(
             response => response.json()).then(
                 data => {
-        console.log(data);
+                    document.getElementById("ville").innerHTML=data.name;
+                    document.getElementById("pays").innerHTML=data.sys.country;
+
+                    console.log(data);
     })
 }
 
